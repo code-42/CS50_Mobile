a0e5fae079095ce6f210b035c30bf6436dd66db5
Edward Dupre <edwd42@gmail.com>
2018-08-16 19:22:53 -0400

    increment item-count

--- project0/script.js
+++ project0/script.js
@@ -3,12 +3,17 @@ const classNames = {
   TODO_CHECKBOX: 'todo-checkbox',
   TODO_TEXT: 'todo-text',
   TODO_DELETE: 'todo-delete',
-}
+};
 
-const list = document.getElementById('todo-list')
-const itemCountSpan = document.getElementById('item-count')
-const uncheckedCountSpan = document.getElementById('unchecked-count')
+const list = document.getElementById('todo-list');
+const itemCountSpan = document.getElementById('item-count');
+const uncheckedCountSpan = document.getElementById('unchecked-count');
+var id = 0;
 
 function newTodo() {
-  alert('New TODO button clicked!')
+  // alert('New TODO button clicked!')
+    // increment item count
+    document.getElementById('item-count').innerText++;
+
 }
+
