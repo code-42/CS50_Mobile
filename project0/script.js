7ff03889b08df6aff412bef950b383196fa193f7
Edward Dupre <edwd42@gmail.com>
2018-08-16 20:20:33 -0400

    add checkbox next to todo list

--- project0/script.js
+++ project0/script.js
@@ -11,15 +11,25 @@ const uncheckedCountSpan = document.getElementById('unchecked-count');
 var id = 0;
 
 function newTodo() {
-  // alert('New TODO button clicked!')
-    // increment item count
+
+    // increment item-count
     document.getElementById('item-count').innerText++;
 
     // add li for new todo
     var ul = document.getElementById('todo-list');
     var li = document.createElement('li');
-    li.appendChild(document.createTextNode("new todo"));
     li.setAttribute('id', id++);
+
+    // add a checkbox next to li
+    var checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.name = 'name';
+    checkbox.value = 'value';
+    checkbox.id = 'id';
+
+    // add the checkbox and todo item to ul
+    li.appendChild(checkbox);
+    li.appendChild(document.createTextNode("new todo " + id));
     ul.appendChild(li);
 
 }
